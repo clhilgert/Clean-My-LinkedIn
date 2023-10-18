7f6dd7eab370110393aafc9ae21be876480c7a77
clhilgert <claytonhilgertmst@gmail.com>
2023-10-18 16:04:55 -0500

    add state to sliders

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,74 @@
+const promotedCheckbox = document.getElementById("promoted");
+const suggestedCheckbox = document.getElementById("suggested");
+
+function saveState() {
+  chrome.storage.sync.set({
+    promotedChecked: promotedCheckbox.checked,
+    suggestedChecked: suggestedCheckbox.checked,
+  });
+}
+
+function loadState() {
+  chrome.storage.sync.get({
+    promotedChecked: false,
+    suggestedChecked: false,
+  }, (items) => {
+    if (promotedCheckbox) {
+      promotedCheckbox.checked = items.promotedChecked;
+    }
+    if (suggestedCheckbox) {
+      suggestedCheckbox.checked = items.suggestedChecked;
+    }
+  });
+}
+
+if (promotedCheckbox) {
+  promotedCheckbox.addEventListener("click", () => {
+    findAndDeleteRelative(document);
+    saveState();
+  });
+}
+
+if (suggestedCheckbox) {
+  suggestedCheckbox.addEventListener("click", () => {
+    saveState();
+  });
+}
+
+window.addEventListener('load', () => {
+  loadState();
+});
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message === "popupOpened") {
+    loadState();
+  }
+});
+
 function findAndDeleteRelative(targetNode) {
   const spans = targetNode.querySelectorAll('span');
 
-  for (const span of spans) {
-    if (span.textContent.includes("Promoted")) {
-      let currentNode = span.parentElement;
-      while (currentNode) {
-        if (currentNode.classList.contains("full-height")) {
-          if (currentNode.parentNode) {
-            currentNode.parentNode.removeChild(currentNode);
+  if (promotedCheckbox.checked) {
+    console.log(promotedCheckbox, " in function");
+    for (const span of spans) {
+      if (span.textContent.includes("Promoted")) {
+        let currentNode = span.parentElement;
+        while (currentNode) {
+          if (currentNode.classList.contains("full-height")) {
+            if (currentNode.parentNode) {
+              currentNode.parentNode.removeChild(currentNode);
+            }
+            break;
           }
-          break;
+          currentNode = currentNode.parentElement;
         }
-        currentNode = currentNode.parentElement;
       }
     }
   }
 }
 
 window.addEventListener('load', () => {
+  console.log(promotedCheckbox, " outside function");
   findAndDeleteRelative(document);
 
   const observer = new MutationObserver((mutationsList, observer) => {
